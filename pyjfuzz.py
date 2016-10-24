250e90a53cfed2ce8c63e0aeacaf7b46b97fcd67
Daniele Linguaglossa <danielelinguaglossa@gmail.com>
2016-10-24 15:28:46 +0200

    Addedd behavior based fuzzing

--- pyjfuzz.py
+++ pyjfuzz.py
@@ -6,7 +6,7 @@ PyJFuzz trivial python fuzzer based on radamsa.
 
 MIT License
 
-Copyright (c) 2016 Daniele Linguaglossa <danielelinguaglossa@gmail.com>
+Copyright (c) 2016 Daniele Linguaglossa <d.linguaglossa@mseclab.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
