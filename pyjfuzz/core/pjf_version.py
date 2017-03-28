5037f35b9569654fcab14390c3be4711ba5a6a67
Daniele Linguaglossa <d.linguaglossa@consulthink.it>
2017-03-29 01:25:00 +0200

    version 1.1.1

--- pyjfuzz/core/pjf_version.py
+++ pyjfuzz/core/pjf_version.py
@@ -27,7 +27,7 @@ PYJFUZZ_LOGLEVEL = logging.INFO
 
 PYJFUZZ_COMPANY = 'Mobile Security Lab 2016'
 
-PYJFUZZ_VERSION = '1.1.0'
+PYJFUZZ_VERSION = '1.1.1'
 
 PYJFUZZ_AUTHOR = "Daniele 'dzonerzy' Linguaglossa"
 
